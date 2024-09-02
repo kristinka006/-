f2edcf712d20507cae1dbfa2b23f2971dceb949a
kristinka006 <149041351+kristinka006@users.noreply.github.com>
2024-09-02 14:20:00 +0300

    Update ксющка и крыстынка ис 32.py

--- ксющка и крыстынка ис 32.py
+++ ксющка и крыстынка ис 32.py
@@ -1,20 +1,21 @@
 while True:
     try:
-        a= int(input("Введите первую циферку: "))
-        b= int(input("Введите вторую циферку: "))
-        act = input("Выберите дальнейшее действие: (+, -, *, /,):")
+        a= int(input("Введите первое число: "))
+        b= int(input("Введите второе число: "))
+        act = input("Выберите дальнейшее действие: (+, -, *, /, **, S):")
     except:
-        print("Упс, что-то не так")
+        print("Ошибка")
     if act == '+':
         print(a+b)
-        break
     elif act == '-':
         print(a-b)
-        break
     elif act == '*':
         print(a*b)
-        break
     elif act == '/':
         print(a/b)
-        break
-   
+    elif act == '**':
+        print(a**b)
+    elif act == 'S':
+         s = int(a)**(0.5)
+         answer = (b)**(0.5)
+         print(s,answer)
